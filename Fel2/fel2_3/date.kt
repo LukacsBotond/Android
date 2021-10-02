3b81b96020c16f3a68115f294df9b538a1149d3f
Lukacs <lukacsbotond@gmail.com>
2021-10-02 18:39:13 +0300

    fel3

--- Fel2/fel2_3/date.kt
+++ Fel2/fel2_3/date.kt
@@ -1,8 +1,39 @@
-data class Date(val year: Int, val month: Int, val day: Int) {}
+import kotlin.Comparable
+
+data class Date(val year: Int, val month: Int, val day: Int) : Comparable<Date>, Comparator<Date>{
+    override operator fun compareTo(other: Date) : Int = when {
+		year != other.year -> year - other.year
+		month != other.month -> month - other.month
+		else -> day - other.day
+	}
+
+    override fun compare(p0: Date, p1: Date): Int{
+        if( p0.month < p1.month){
+            return -1;
+        }
+        if(p0.month == p1.month) return 0;
+        return 1;
+    }
+
+}
 
 fun Date.checkLeap(): Boolean {
-    if ((this.year % 100 == 0 && this.year % 400 != 0) && this.year % 4 == 0) {
-        return true
+    if (this.year % 4 == 0) {
+        if (this.year % 100 == 0 && this.year % 400 != 0) return true
     }
     return false
 }
+
+fun Date.vailidDate(): Boolean {
+    val dates = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    if (this.month > 12 || this.month < 1) return false;
+    if (this.month == 2) {
+        if (this.checkLeap() && this.day > 29) return false;
+    }
+    if (this.day > dates[this.month-1] || this.day < 0) return false;
+
+    return true;
+}
+
+
+
--- Fel2/fel2_3/main.kt
+++ Fel2/fel2_3/main.kt
@@ -1,5 +1,36 @@
 fun main() {
 
-    var d = Date(2012, 1, 1)
-    println(d.checkLeap())
+    var d = Date(2000, 1, 1)
+    println("$d leap year? ${d.checkLeap()}")
+    println("$d valid date? ${d.vailidDate()}")
+
+    var validNr = 0;
+    var dates = mutableListOf<Date>();
+    while(validNr <= 10){
+        //println("try:")
+        val year = (0..3000).random();
+        val month = (1..30).random();
+        val day = (1..50).random();
+        //println("$year $month $day")
+        val randDate = Date( year,month,day  )
+        if(randDate.vailidDate()){
+            dates.add(randDate);
+            validNr++;
+        }
+        else{
+            println(randDate)
+        }
+    }
+    println("Correct dates")
+    dates .forEach { println(it) }
+    println("Sort")
+    dates.sortBy{ it } 
+    dates.forEach{println(it)}
+    println("reverse Sort")
+    dates.sortByDescending { it }
+    dates.forEach{println(it)}
+    println("my sort")
+    dates.sortWith( compareBy { it.month }  )
+    dates.forEach{println(it)}
 }
+ 
