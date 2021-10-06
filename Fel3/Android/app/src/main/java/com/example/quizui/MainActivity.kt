d1f8ebe1474736554d545e4c66e3163b7a42010d
Lukacs <lukacsbotond@gmail.com>
2021-10-06 20:15:04 +0300

    Android logging

--- Fel3/Android/app/src/main/java/com/example/quizui/MainActivity.kt
+++ Fel3/Android/app/src/main/java/com/example/quizui/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.quizui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClicked = findViewById<Button>(R.id.button)
+
+        btnClicked.setOnClickListener {
+
+            Toast.makeText(this@MainActivity, "Button clicked", Toast.LENGTH_SHORT).show()
+            Log.i("mainActivity","clicked")
+        }
     }
 }
